8d839d3a9d306ba05413a2dd6e9acb82d53c47a5
Neha Sinha <neha182693@gmail.com>
2026-03-09 19:33:34 +0530

    modify main method to run clean code example

--- src/main/java/chapter1/Main.java
+++ src/main/java/chapter1/Main.java
@@ -3,6 +3,9 @@ package main.java.chapter1;
 public class Main {
 
 public static void main (String[] args) {
-    new O().p(2,5);
+    System.out.print("Bad code result : ");
+    new O().p(2, 5);
+    System.out.println("Clean code result : "+new OrderCalculator().calculateTotal(2, 5));
+
     }
 }
