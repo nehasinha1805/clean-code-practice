4d3b990a843490809f134537e09bf82ea2fe869f
Neha Sinha <neha182693@gmail.com>
2026-03-10 18:13:38 +0530

    add bad pronunciation variable names class

---
+++ src/main/java/chapter2/DtaRcrd102.java
@@ -0,0 +1,11 @@
+package main.java.chapter2;
+
+import java.util.Date;
+
+public class DtaRcrd102 {
+
+    // bad pronunciation variable names
+    private Date genymdhms; // generation date, year, month, day, hour, minute, and second
+    private Date modymdhms;
+    private final String pszqint = "102";
+}
