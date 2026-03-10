3d72235f95f9d7e02bfc4c95f8ad914e3e5bd9d4
Neha Sinha <neha182693@gmail.com>
2026-03-10 18:16:23 +0530

    add good pronunciation variable names class

---
+++ src/main/java/chapter2/Customer.java
@@ -0,0 +1,11 @@
+package main.java.chapter2;
+
+import java.util.Date;
+
+public class Customer {
+
+    // Used Pronounceable Names
+    private Date generationTimestamp;
+    private Date modificationTimestamp;
+    private final String recordId = "102";
+}
